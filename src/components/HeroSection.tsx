425379ff341fec7295fbffebeea39bffd963e287
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:24:29 +0000

    HeroSection.tsx: changed

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,181 @@
+import * as React from 'react';
+import Markdown from 'markdown-to-jsx';
+import classNames from 'classnames';
+import { getComponent } from '../../components-registry';
+import { mapStylesToClassNames as mapStyles } from '../../utils/map-styles-to-class-names';
+
+export default function HeroSection(props) {
+    const cssId = props.elementId || null;
+    const colors = props.colors || 'colors-a';
+    const sectionStyles = props.styles?.self || {};
+    const sectionBorderWidth = sectionStyles.borderWidth ? sectionStyles.borderWidth : 0;
+    return (
+        <div
+            id={cssId}
+            className={classNames(
+                'sb-component',
+                'sb-component-section',
+                'sb-component-hero-section',
+                colors,
+                'flex',
+                'flex-col',
+                'justify-center',
+                'relative',
+                sectionStyles.height ? mapMinHeightStyles(sectionStyles.height) : null,
+                sectionStyles.margin,
+                sectionStyles.padding,
+                sectionStyles.borderColor,
+                sectionStyles.borderRadius ? mapStyles({ borderRadius: sectionStyles.borderRadius }) : null,
+                sectionStyles.borderStyle ? mapStyles({ borderStyle: sectionStyles.borderStyle }) : null
+            )}
+            style={{
+                borderWidth: `${sectionBorderWidth}px`
+            }}
+            data-sb-field-path={props.annotationPrefix}
+        >
+            {props.backgroundImage && heroBackgroundImage(props.backgroundImage)}
+            <div
+                className={classNames(
+                    'flex',
+                    'relative',
+                    'w-full',
+                    sectionStyles.justifyContent ? mapStyles({ justifyContent: sectionStyles.justifyContent }) : null
+                )}
+            >
+                <div className={classNames('w-full', sectionStyles.width ? mapMaxWidthStyles(sectionStyles.width) : null)}>
+                    <div
+                        className={classNames(
+                            'flex',
+                            '-mx-4',
+                            sectionStyles.flexDirection ? mapFlexDirectionStyles(sectionStyles.flexDirection) : null,
+                            sectionStyles.alignItems ? mapStyles({ alignItems: sectionStyles.alignItems }) : null
+                        )}
+                    >
+                        <div className="my-3 flex-1 px-4 w-full">
+                            {heroBody(props)}
+                            {heroActions(props)}
+                        </div>
+                        {props.feature && (
+                            <div className="my-3 flex-1 px-4 w-full">
+                                <div data-sb-field-path=".feature">{heroFeature(props.feature)}</div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function heroFeature(feature) {
+    const featureType = feature.type;
+    if (!featureType) {
+        throw new Error(`hero section feature does not have the 'type' property`);
+    }
+    const Feature = getComponent(featureType);
+    if (!Feature) {
+        throw new Error(`no component matching the hero section feature type: ${featureType}`);
+    }
+    return <Feature {...feature} />;
+}
+
+function heroBackgroundImage(image) {
+    const imageUrl = image.url;
+    if (!imageUrl) {
+        return null;
+    }
+    const imageStyles = image.styles?.self || {};
+    const imageOpacity = imageStyles.opacity || imageStyles.opacity === 0 ? imageStyles.opacity : 100;
+    return (
+        <span
+            className="bg-cover bg-center block absolute inset-0"
+            style={{
+                backgroundImage: `url('${imageUrl}')`,
+                opacity: imageOpacity * 0.01
+            }}
+            data-sb-field-path=".backgroundImage.url#@style"
+        />
+    );
+}
+
+function heroBody(props) {
+    const styles = props.styles || {};
+    return (
+        <div>
+            {props.title && (
+                <h2 className={classNames('text-4xl', 'sm:text-5xl', styles.title ? mapStyles(styles.title) : null)} data-sb-field-path=".title">
+                    {props.title}
+                </h2>
+            )}
+            {props.subtitle && (
+                <p className={classNames('text-xl', 'sm:text-2xl', styles.subtitle ? mapStyles(styles.subtitle) : null)} data-sb-field-path=".subtitle">
+                    {props.subtitle}
+                </p>
+            )}
+            {props.text && (
+                <Markdown
+                    options={{ forceBlock: true, forceWrapper: true }}
+                    className={classNames('sb-markdown', 'md:text-lg', styles.text ? mapStyles(styles.text) : null)}
+                    data-sb-field-path=".text"
+                >
+                    {props.text}
+                </Markdown>
+            )}
+        </div>
+    );
+}
+
+function heroActions(props) {
+    const actions = props.actions || [];
+    if (actions.length === 0) {
+        return null;
+    }
+    const styles = props.styles || {};
+    const Action = getComponent('Action');
+    return (
+        <div
+            className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', styles.actions ? mapStyles(styles.actions) : null)}
+            data-sb-field-path=".actions"
+        >
+            {actions.map((action, index) => (
+                <Action key={index} {...action} className="mb-3 mx-2 lg:whitespace-nowrap" annotationPrefix={`.${index}`} />
+            ))}
+        </div>
+    );
+}
+
+function mapMinHeightStyles(height) {
+    switch (height) {
+        case 'auto':
+            return 'min-h-0';
+        case 'screen':
+            return 'min-h-screen';
+    }
+    return null;
+}
+
+function mapMaxWidthStyles(width) {
+    switch (width) {
+        case 'narrow':
+            return 'max-w-screen-md';
+        case 'wide':
+            return 'max-w-screen-xl';
+        case 'full':
+            return 'max-w-full';
+    }
+    return null;
+}
+
+function mapFlexDirectionStyles(flexDirection) {
+    switch (flexDirection) {
+        case 'row':
+            return ['flex-col', 'lg:flex-row'];
+        case 'row-reverse':
+            return ['flex-col-reverse', 'lg:flex-row-reverse'];
+        case 'col':
+            return ['flex-col'];
+        case 'col-reverse':
+            return ['flex-col-reverse'];
+    }
+    return null;
+}
