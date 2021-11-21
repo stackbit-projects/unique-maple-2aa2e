90015c0b1d125dbc81cfbd3a67cbe539209a7213
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:46:18 +0000

    ImageBlock.tsx: added

---
+++ src/components/ImageBlock.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import classNames from 'classnames';
+
+export default function ImageBlock(props) {
+    const { url, altText, caption } = props;
+    if (!url) {
+        return null;
+    }
+    const cssClasses = props.className || null;
+    const cssId = props.elementId || null;
+    const styles = props.styles?.self || {};
+    const imageOpacity = styles.opacity || styles.opacity === 0 ? styles.opacity : 100;
+
+    return (
+        <img
+            id={cssId}
+            className={classNames('sb-component', 'sb-component-block', 'sb-component-image-block', cssClasses)}
+            src={url}
+            alt={altText || ''}
+            title={caption}
+            style={{ opacity: imageOpacity * 0.01 }}
+            data-sb-field-path=".url#@src .altText#@alt .caption#@title .elementId#@id"
+        />
+    );
+}
