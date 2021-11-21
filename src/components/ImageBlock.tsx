295146c70ce3106017529ccfe7fd6791c059ee3f
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:47:29 +0000

    ImageBlock.tsx: changed

--- src/components/ImageBlock.tsx
+++ src/components/ImageBlock.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import classNames from 'classnames';
-import 
+import {}
 
 export default function ImageBlock(props) {
     const { url, altText, caption } = props;
