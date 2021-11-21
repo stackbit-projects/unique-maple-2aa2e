f53b63b4f8b654709f6f89c7c701704bfcaec901
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:34:20 +0000

    HeroSection.tsx: changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -42,7 +42,9 @@ export default function HeroSection(props) {
                     sectionStyles.justifyContent ? mapStyles({ justifyContent: sectionStyles.justifyContent }) : null
                 )}
             >
-                <div className={classNames('w-full', sectionStyles.width ? mapMaxWidthStyles(sectionStyles.width) : null)}>
+                <div
+                    data-sb-field-path=".contentStyles"
+                    className={classNames('w-full', sectionStyles.width ? mapMaxWidthStyles(sectionStyles.width) : null)}>
                     <div
                         className={classNames(
                             'flex',
