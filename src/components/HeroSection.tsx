dba589ef5bbb59e49c2e168ca5f25a1e30728a51
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:25:08 +0000

    HeroSection.tsx: changed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
 import { getComponent } from '@stackbit/components';
-import { mapStylesToClassNames as mapStyles } from '@stackbit/components/utils/map-styles-to-class-names';
+import { mapStylesToClassNames as mapStyles } from '@stackbit/components/dist/utils/map-styles-to-class-names';
 
 export default function HeroSection(props) {
     const cssId = props.elementId || null;
