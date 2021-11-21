c319df44139e4bedc17d60810f2e02c31cade080
Stackbit Code Editor <projects@stackbit.com>
2021-11-21 13:49:16 +0000

    ImageBlock.tsx: changed

--- src/components/ImageBlock.tsx
+++ src/components/ImageBlock.tsx
@@ -11,7 +11,7 @@ export default function ImageBlock(props) {
     const cssId = props.elementId || null;
     const styles = props.styles?.self || {};
     const imageOpacity = styles.opacity || styles.opacity === 0 ? styles.opacity : 100;
-    const justifyContent = mapStylesToClassNames({ justifyContent: styles.justifyContent });
+    const justifyContent = styles.justifyContent ? mapStylesToClassNames({ justifyContent: styles.justifyContent }) : null;
 
     return (
         <img
